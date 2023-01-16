0dd2ed892118f4e17bb65bffead080294b4d93c6
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2023-01-16 18:06:20 +0300

    ds

--- teacher/views.py
+++ teacher/views.py
@@ -103,7 +103,7 @@ def teacher_question_file_view(request):
             question.save()       
         else:
             print("form is invalid")
-        return HttpResponseRedirect('/teacher/teacher-view-question')
+        return HttpResponseRedirect('/teacher/teacher-view-question-file')
     return render(request,'teacher/teacher_questionfile.html',{'form':questionForm})
 
 
