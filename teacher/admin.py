874b303fb29c6aa484bfb61f90c6b48878f832ca
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2023-01-16 18:15:09 +0300

    df

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from exam.models import QuestionSheet,StudentAnswer
 
 # Register your models here.
+
+admin.site.register(QuestionSheet)
+admin.site.register(StudentAnswer)
